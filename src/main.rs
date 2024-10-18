95e255510c35cd7312df6212fa23bdb00d55f628
Kevin F. CALMO <kevinfcalmo@gmail.com>
2024-10-17 22:48:18 -0400

    Test data in function main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use inventory::Inventory;
+
+mod inventory;
+mod item;
+
 fn main() {
-    println!("Hello, world!");
+    let mut inventory = Inventory { items: Vec::new() };
+    let new_item = item::Item::create_item("Glace", 6.50, 12, item::Category::Drink);
+    inventory.add_new_item(new_item);
+
+    println!("{:?}", inventory.items[0].get_categorie());
+    inventory.items[0].set_categorie(&item::Category::Food);
+    println!("new categorie => {:?}", inventory.items[0].get_categorie());
 }
